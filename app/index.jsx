5826ab8a3149b70e5e9db212257b5b713526f1bd
“KayixNi” <cirokayix53@gmail.com>
2025-08-22 16:46:22 +0800

    index is changed

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y  G O A L S
+        D I Y M O T O
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
